ae4947b1700d49b187ce99fa77fb973a1fdc71ad
thom6530 <thom6530@stthomas.edu>
2025-12-10 13:12:47 -0600

    Adding CORS support

--- rest_server.mjs
+++ rest_server.mjs
@@ -253,6 +253,17 @@ app.delete('/remove-incident', async (req, res) => {
     }
 });
 
+// Add CORS support - allow frontend to make requests
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 /********************************************************************
  ***   START SERVER                                               *** 
  ********************************************************************/
